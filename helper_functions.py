e0f39ef0baba569be60398f2916ce8a2224884a1
Prashant Malani <p.malani@gmail.com>
2013-12-08 03:20:18 -0800

    Add valid_words function
    Add a helper function which takes a rack in
    string form, and returns the list of valid words.
    
    Also add a sample txt file to test out the function.
    
    Signed-off-by: Prashant Malani <p.malani@gmail.com>

--- helper_functions.py
+++ helper_functions.py
@@ -2,13 +2,14 @@
 #
 # Author: Prashant Malani <p.malani@gmail.com
 
+filename = 'sample_list.txt'
 
 def get_word_list():
   """ This function reads the  input file of words, and outputs
   a list of all the words. We can assume that the file format
   is of one word per line, new-line at the end of each line.
   """
-  file = open('sowpods.txt', 'r')
+  file = open(filename, 'r')
   word_list = []
   for line in file:
     new_word = line.strip('\n')
@@ -16,3 +17,31 @@ def get_word_list():
     word_list.append(final_word)
   file.close()
   return word_list
+
+
+def valid_words(word_list, rack):
+  """ This function takes in the rack of letters, and finds which
+  valid words can be created from that list.
+  Rack of letters is in STRING form.
+  Returns a list of valid words """
+  valid_list = []
+
+  # Iterate through every word, and see it can be made from rack
+  for word in word_list:
+    print word
+    valid = True
+    used_indices = []
+    temp_rack =  list(rack)
+    # See if every letter of our word is in the rack.
+    # Keep deleting elements found from the rack, to account
+    # for repeat letters.
+    for i in range(len(word)):
+      if word[i] in temp_rack:
+        index = temp_rack.index(word[i])
+        temp_rack.pop(index)
+      else:
+        valid = False
+        break
+    if valid ==  True:
+      valid_list.append(word)
+  return valid_list
